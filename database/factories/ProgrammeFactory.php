d8e7b89a674a70f1062fbcd7b0f09953b652520e
Joaquin Pino <joaquin.pino@crowd2fund.com>
2018-12-03 20:37:17 +0000

    Generate dummy data. Not mandatory

--- database/factories/ProgrammeFactory.php
+++ database/factories/ProgrammeFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Programme::class, function (Faker $faker) {
     return [
-        //
+        'title'     => $faker->name,
+        'channel'   => $faker->name,
+        'repeat'    => $faker->boolean,
+        'genre'     => $faker->name,
+        'broadcast' => $faker->dateTime,
     ];
 });
