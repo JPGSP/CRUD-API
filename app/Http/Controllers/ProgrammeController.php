3a82a50cdd08cbd93b57bb7df409f467e164509f
Joaquin Pino <joaquin.pino@crowd2fund.com>
2018-12-03 21:41:22 +0000

    Update existing methods.Validation added.

--- app/Http/Controllers/ProgrammeController.php
+++ app/Http/Controllers/ProgrammeController.php
@@ -96,26 +96,58 @@ class ProgrammeController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * @param Request $request
+     * @param int     $id
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function edit($id)
+    public function update(Request $request, int $id): JsonResponse
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required|unique:programmes',
+            'channel'   => 'required',
+            'broadcast' => 'required',
+            'repeat'    => 'required',
+            'genre'     => 'nullable|string',
+        ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+
+            return response()->json([
+                'message' => $errors,
+                'data'    => 'Request unsuccessfull',
+                'status'  => Response::HTTP_BAD_REQUEST,
+            ]);
+        } else {
+            /** @var Programme $programme */
+            $programme = Programme::find($id);
+
+            if (empty($programme)) {
+                $response = response()->json([
+                    'message' => 'Resource not found',
+                    'data'    => null,
+                    'status'  => Response::HTTP_NOT_FOUND,
+                ]);
+            } else {
+                $dataRead = $request->all();
+
+                $programme->setTitle($dataRead['title']);
+                $programme->setChannel($dataRead['channel']);
+                $programme->setBroadcast($dataRead['broadcast']);
+                $programme->setRepeat($dataRead['repeat']);
+                $programme->setGenre($dataRead['genre']);
+                $programme->save();
+
+                $response = response()->json([
+                    'message' => 'Request succesfull',
+                    'data'    => $programme,
+                    'status'  => Response::HTTP_OK,
+                ]);
+            }
+        }
+
+        return $response;
     }
 
     /**
